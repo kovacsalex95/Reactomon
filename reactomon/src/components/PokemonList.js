4ba14b7010f4dbf1611113c444d498864127b2ec
Alex Kovács <kovacsalex95@gmail.com>
2021-09-06 14:11:27 +0200

    Update PokemonList.js

--- reactomon/src/components/PokemonList.js
+++ reactomon/src/components/PokemonList.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class PokemonList extends Component {
 
     render() {
 
-        /*const listItems = this.props.items.map((item) => 
-            <div className="pokemon">
-                <a class="name" href={"/pokemon/" + item.id}>{item.name}</a>
-                <img src={item.image} alt={item.name}></img>
+        const results = this.props.items.results === undefined ? [] : this.props.items.results;
+
+        const listItems = results.map((item) => 
+            <div key={item.url} className="pokemon">
+                <Link to={"/pokemon/" + getIDfromURL(item.url)}>{item.name}</Link>
             </div>
         );
 
         return (
             <div className="pokemon-list">{listItems}</div>
-        )*/
-        return(
-            <pre>{this.props.items}</pre>
         )
     }
+}
+
+/**
+ * @param {String} url The url itself
+ */
+function getIDfromURL(url)
+{
+    const parts = url.split('/');
+    const id = parts[parts.length - 2];
+    return id;
 }
 
 export default PokemonList;
